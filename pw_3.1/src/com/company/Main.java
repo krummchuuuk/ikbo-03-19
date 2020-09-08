5c5b62d0e106d27e349f2c0524704ffa235d7ef6
Арина Крумм <70585905+krummchuuuk@users.noreply.github.com>
2020-09-08 14:38:15 +0300

    Add files via upload

---
+++ pw_3.1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+abstract class Shape {}
+
+abstract class Circle extends Shape {};
+abstract class Rectangle extends Shape {};
+abstract class Square extends Rectangle{};
+
+public class Main {
+
+    public static void main(String[] args) {}
+}
